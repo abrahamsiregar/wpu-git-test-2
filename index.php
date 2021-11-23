4253fd6380a382549b24a9f144c99ada7c30a568
abrahamsiregar <bramw.siregar18@gmail.com>
2021-11-23 10:26:42 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, quo velit! Magni impedit amet eius libero excepturi? Dolorum eius consequuntur quod rem cum fuga accusamus, eveniet alias corporis asperiores delectus!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
